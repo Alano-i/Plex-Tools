ecad55f7fd227e5aacb7113c13ea43ea642426c3
Alano-i <68833595+Alano-i@users.noreply.github.com>
2023-03-02 20:38:12 +0800

    Update __init__.py

--- plugins/plexsortout/__init__.py
+++ plugins/plexsortout/__init__.py
@@ -1,21 +1,19 @@
-
 from mbot.openapi import mbot_api
 from mbot.openapi import media_server_manager
 
-#导入插件类
+# 导入插件类
 from .plexsortout import plexsortout
 plexst = plexsortout()
 
-#获取MR服务
+# 获取MR服务
 mrserver = mbot_api
 
-#获取plex媒体库
+# 获取plex媒体库
 plexserver = media_server_manager.master_plex.plex
 servertype='plex'
 
-#设置服务参数
+# 设置服务参数
 plexst.setdata(plexserver,mrserver,servertype)
-
 
 from .event import *
 from .command import *
