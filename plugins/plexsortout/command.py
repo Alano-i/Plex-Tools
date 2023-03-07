82bfd80cf088b28346f3b8450cac296cbd9bec61
Alano-i <68833595+Alano-i@users.noreply.github.com>
2023-03-07 13:52:11 +0800

    [update] TOP250 每天刷新最新数据

--- plugins/plexsortout/command.py
+++ plugins/plexsortout/command.py
@@ -2,11 +2,12 @@ from mbot.core.plugins import plugin, PluginCommandContext, PluginCommandRespons
 from . import plexst
 from mbot.openapi import mbot_api
 from mbot.core.params import ArgSchema, ArgType
-
+from .get_top250 import get_top250
 import logging
+
+server = mbot_api
 _LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
-
 plugins_name = '「PLEX 工具箱」'
 
 def get_enum_data():
@@ -33,7 +34,7 @@ is_lock_list = [
     }
 ]
     
-@plugin.command(name='select_data', title='Plex整理全库', desc='自动翻译标签&&拼音排序&&添加TOP250标签', icon='HourglassFull',run_in_background=True)
+@plugin.command(name='select_data', title='整理 PLEX 媒体库', desc='自动翻译标签 & 拼音排序 & 添加TOP250标签 & 筛选Fanart封面', icon='HourglassFull',run_in_background=True)
 def select_data(ctx: PluginCommandContext,
                 library: ArgSchema(ArgType.Enum, '选择需要整理的媒体库', '', enum_values=get_enum_data,multi_value=True),
                 sortoutNum: ArgSchema(ArgType.String, '整理数量，示例：50，表示只整理最新的50条，留空整理全部', '', default_value='ALL', required=False),
@@ -46,14 +47,16 @@ def select_data(ctx: PluginCommandContext,
     if user_list:
         for user in user_list:
             if is_lock == 'run_all':
-                mbot_api.notify.send_system_message(user.uid, '手动运行 Plex 媒体库整理',
-                                                    '翻译标签 && 拼音排序 && 添加TOP250标签完毕')
+                mbot_api.notify.send_system_message(user.uid, '手动运行整理 PLEX 媒体库', '翻译标签 && 拼音排序 && 添加TOP250标签完毕 && 筛选Fanart封面')
             else:
-                mbot_api.notify.send_system_message(user.uid, '手动运行 Plex 媒体库整理',
-                                                '锁定 PLEX 海报和背景完毕')
-    return PluginCommandResponse(True, f'手动运行 Plex 媒体库整理完成')
+                mbot_api.notify.send_system_message(user.uid, '手动运行整理 PLEX 媒体库', '锁定 PLEX 海报和背景完毕')
+    return PluginCommandResponse(True, f'手动运行整理 PLEX 媒体库完成')
     
-
+@plugin.command(name='get_top250', title='更新 TOP250 列表', desc='获取最新豆瓣和IMDB TOP250 列表', icon='MovieFilter', run_in_background=True)
+def get_top250_echo(ctx: PluginCommandContext):
+    _LOGGER.info(f'{plugins_name}开始手动获取最新 TOP250 列表')
+    get_top250()
+    _LOGGER.info(f'{plugins_name}手动获取最新 TOP250 列表完成')
 
 # @plugin.command(name='plexcollection', title='Plex整理合集首字母', desc='自动整理合集首字母', icon='HourglassFull',run_in_background=True)
 # def echo_c(ctx: PluginCommandContext):
@@ -64,3 +67,4 @@ def select_data(ctx: PluginCommandContext,
 #             mbot_api.notify.send_system_message(user.uid, 'Plex整理合集首字母',
 #                                                 '自动整理合集首字母')
 #     return PluginCommandResponse(True, f'Plex合集整理完成')
+
