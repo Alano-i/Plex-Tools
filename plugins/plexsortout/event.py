c999c2b071a87aa403e16928e509ef2eae5fbbfe
Alano-i <68833595+Alano-i@users.noreply.github.com>
2023-06-28 18:29:06 +0800

    Update event.py

--- plugins/plexsortout/event.py
+++ plugins/plexsortout/event.py
@@ -7,7 +7,7 @@ from mbot.register.controller_register import login_required
 import json
 import time
 import threading
-# from plexapi.server import PlexServer
+from plexapi.server import PlexServer
 
 from mbot.core.event.models import EventType
 from mbot.core.plugins import PluginContext,PluginMeta,plugin
@@ -32,6 +32,13 @@ def after_setup(plugin: PluginMeta, plugin_conf: dict):
     _LOGGER.info(f'{plugins_name}插件开始加载')
     # global added, libtable , plex_url, plex_token
     global added,collection_on,libtable,libstr
+    global plex_url, plex_token,custom_url,mbot_url,mbot_api_key,check
+    plex_url = plugin_conf.get('plex_url','')
+    plex_token = plugin_conf.get('plex_token','')
+    custom_url = plugin_conf.get('custom_url','')
+    mbot_url = plugin_conf.get('mbot_url','')
+    mbot_api_key = plugin_conf.get('mbot_api_key','')
+    check = plugin_conf.get('check',True)
     added = plugin_conf.get('Added') if plugin_conf.get('Added') else None
     collection_on = plugin_conf.get('Collection') if plugin_conf.get('Collection') else None
     if added:
@@ -74,6 +81,13 @@ def config_changed(plugin_conf: dict):
     """
     _LOGGER.info(f'{plugins_name}配置发生变更，加载新配置')
     global added,collection_on,libtable,libstr
+    global plex_url, plex_token,custom_url,mbot_url,mbot_api_key,check
+    plex_url = plugin_conf.get('plex_url','')
+    plex_token = plugin_conf.get('plex_token','')
+    custom_url = plugin_conf.get('custom_url','')
+    mbot_url = plugin_conf.get('mbot_url','')
+    mbot_api_key = plugin_conf.get('mbot_api_key','')
+    check = plugin_conf.get('check',True)
     added = plugin_conf.get('Added') if plugin_conf.get('Added') else None
     collection_on = plugin_conf.get('Collection') if plugin_conf.get('Collection') else None
     if added:
@@ -112,6 +126,37 @@ def config_changed(plugin_conf: dict):
 def printAllMembers(cls):
     print('\n'.join(dir(cls)))
 
+def set_plex():
+    if  plex_url and plex_token:
+        plexserver = PlexServer(plex_url, plex_token)
+        # settings = plexserver.settings
+        # 开启GDM网络发现
+        plexserver.settings.get('gdmEnabled').set(True)
+        if custom_url:
+            if plexserver.settings.get('customConnections').value != custom_url:
+                # 设置自定义域名
+                plexserver.settings.get('customConnections').set(custom_url)
+        # 开启webhooks开关
+        plexserver.settings.get('webHooksEnabled').set(True)
+        # 开启通知推送开关
+        plexserver.settings.get('pushNotificationsEnabled').set(True)
+        plexserver.settings.save()
+
+        if mbot_url and mbot_api_key:
+            webhook_url = f'{mbot_url}/api/plugins/get_plex_event/webhook?access_key={mbot_api_key}'
+            # 自动设置 webhooks
+            account = plexserver.myPlexAccount()
+            webhooks = account.webhooks()
+            if webhook_url not in webhooks:
+                webhooks.append(webhook_url)
+                account.setWebhooks(webhooks)
+                _LOGGER.info(f"{plugins_name} 已向 PLEX 服务器添加 Webhook")
+    else:
+        _LOGGER.error(f'{plugins_name}PLEX URL 或 TOKEN 未设置，无法检查设置')
+
+
+        
+
 last_event_time = 0
 last_event_count = 1
 
@@ -139,25 +184,24 @@ def webhook():
         org_type = metadata.get('type', '')
 
         # 如果是照片库直接跳过
-        if library_section_type == 'photo': return api_result(code=0, message=plex_event, data=data)
-        _LOGGER.info(f'{plugins_name}接收到 PLEX 通过 Webhook 传过来的「入库事件」，开始分析事件')
-        
-        # 执行自动整理
-        # plexst.process(library_section_title)
-        # thread = threading.Thread(target=lambda: plexst.process(library_section_title))
-        thread = threading.Thread(target=plexst.process_new, args=(library_section_title,rating_key,parent_rating_key,grandparent_rating_key,grandparent_title,parent_title,org_title,org_type))
-        thread.start()
+        if library_section_type == 'photo':
+            return api_result(code=0, message=plex_event, data=data)
+        # _LOGGER.info(f'{plugins_name}接收到 PLEX 通过 Webhook 传过来的「入库事件」，开始分析事件')
+    
+        # thread = threading.Thread(target=plexst.process_new, args=(library_section_title,rating_key,parent_rating_key,grandparent_rating_key,grandparent_title,parent_title,org_title,org_type))
+        # thread.start()
 
-        # if time.time() - last_event_time < 75:
-        #     last_event_count = last_event_count + 1
-        #     _LOGGER.info(f'{plugins_name}75 秒内接收到 {last_event_count} 条入库事件，只处理一次')
-        # else:
-        #     last_event_time = time.time()
-        #     last_event_count = 1
-        #     time.sleep(60)
-        #     _LOGGER.info(f'{plugins_name}接收到 PLEX 通过 Webhook 传过来的「入库事件」，开始整理')
-        #     # 执行自动整理
-        #     plexst.process(library_section_title)
+        if time.time() - last_event_time < 15:
+            last_event_count = last_event_count + 1
+            _LOGGER.info(f'{plugins_name}15 秒内接收到 {last_event_count} 条入库事件，只处理一次')
+        else:
+            last_event_time = time.time()
+            last_event_count = 1
+            # time.sleep(60)
+            _LOGGER.info(f'{plugins_name}接收到 PLEX 通过 Webhook 传过来的「入库事件」，开始分析事件')
+            # 执行自动整理
+            thread = threading.Thread(target=plexst.process_new, args=(library_section_title,rating_key,parent_rating_key,grandparent_rating_key,grandparent_title,parent_title,org_title,org_type))
+            thread.start()
 
     return api_result(code=0, message=plex_event, data=data)
 
@@ -189,6 +233,14 @@ def process_recent():
     else:
         libtable=libstr.split(',')
     plexst.process_all(libtable,'10','run_all',0,False,True)
+
+@plugin.task('set_plex', '「检查 PLEX 设置」', cron_expression='15 * * * *')
+def set_plex_ckeck():
+    if check:
+        try:
+            set_plex()
+        except Exception as e:
+            _LOGGER.error(f'{plugins_name}检查 PLEX 服务器设置出错，原因: {e}')
 
 
 # @plugin.on_event(
